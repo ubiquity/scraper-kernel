4b677f3a2c7419425e7d51d8cc8c16397f030fc4
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-13 14:57:59 -0500

    init cmc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-process.exit(0);
+import "source-map-support/register";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import puppeteer from "puppeteer";
+import settings from "./config";
+
+async function scraper(args: typeof process.argv) {
+  const { page, browser } = await browserSetup();
+  const destination = new URL(args[2]);
+  await page.goto(destination.href);
+  await page.close();
+  await browser.close();
+  process.exit(0);
+}
+
+scraper(process.argv);
+
+async function browserSetup() {
+  const browser = await puppeteer.launch(settings);
+  const context = browser.defaultBrowserContext();
+  const page: puppeteer.Page = await context.newPage();
+  return { page, browser };
+}
