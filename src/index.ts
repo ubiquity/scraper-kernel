0807e0f4c30406a827ef6a119f5d465616a4fc66
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-13 16:57:52 -0500

    feature(core): load specific url page logic

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 import puppeteer from "puppeteer";
 import settings from "./config";
+import path from "path";
 
 (async function scraper(args: typeof process.argv) {
   const { browser, page } = await browserSetup();
@@ -23,12 +24,53 @@ async function browserSetup() {
   const browser = await puppeteer.launch(settings);
   const context = browser.defaultBrowserContext();
   const page: puppeteer.Page = await context.newPage();
-  browser.on("targetchanged", globalTargetChangedHandler);
+  // browser.on("targetchanged", globalTargetChangedHandler);
+
+  // @FIXME: TYPING HACK
+  const accepted = async (target: puppeteer.Target | any) => globalTargetChangedHandler(target);
+
+  const rejected = () => {
+    throw new Error("Scrape logic selection timed out");
+  };
+
+  waitForEventWithTimeout(browser, "targetchanged", 10000)
+    .then(accepted, rejected)
+    .then((importedLogic) => importedLogic.default(page));
   return { browser, page };
 }
 
 // This should execute the routine needed based on the page url.
 function globalTargetChangedHandler(target: puppeteer.Target) {
   const url = target.url(); // https://coinmarketcap.com/cryptocurrency-category/
-  console.log(url);
+  const logic = importLogic(url);
+  return logic;
+  // console.log({ url, logic });
+}
+function importLogic(url: string) {
+  const selection = url.split("://").pop();
+  if (!selection) {
+    throw new Error("Page URL parse error");
+  }
+
+  const pathTo = path.join(process.cwd(), "dist", "pages", selection);
+  return import(pathTo);
+}
+
+//
+//
+//
+function waitForEventWithTimeout(eventEmitter: puppeteer.Browser, event: string, timeout: number) {
+  return new Promise(function (resolve, reject) {
+    eventEmitter.on(event, listener);
+    const timer = setTimeout(function () {
+      eventEmitter.off(event, listener);
+      reject(new Error("timeout"));
+    }, timeout);
+
+    function listener(browser: puppeteer.Browser) {
+      clearTimeout(timer);
+      eventEmitter.off(event, listener);
+      resolve(browser);
+    }
+  });
 }
--- src/pages/coinmarketcap.com/cryptocurrency-category/index.ts
+++ src/pages/coinmarketcap.com/cryptocurrency-category/index.ts
@@ -1,48 +1,35 @@
-import Puppeteer from "puppeteer";
-export default async (browser: Puppeteer.Browser) => {
-  //   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  const navigationPromise = page.waitForNavigation();
-
-  await page.goto("https://coinmarketcap.com/view/olympus-pro-ecosystem/");
-
+import puppeteer, { ElementHandle } from "puppeteer";
+export default async (page: puppeteer.Page) => {
+  // const navigationPromise = page.waitForNavigation({ timeout: 10000 });
+  // await navigationPromise;
   await page.setViewport({ width: 1918, height: 1592 });
 
-  await page.waitForSelector(".h7vnx2-1 > .h7vnx2-2 > tbody > tr:nth-child(1) > td:nth-child(3)");
-  await page.click(".h7vnx2-1 > .h7vnx2-2 > tbody > tr:nth-child(1) > td:nth-child(3)");
+  console.trace();
 
-  await page.waitForSelector(".h7vnx2-2 > tbody > tr:nth-child(1) > td > .escjiH");
-  await page.click(".h7vnx2-2 > tbody > tr:nth-child(1) > td > .escjiH");
-
-  await page.waitForSelector("tr:nth-child(1) > td > .sc-16r8icm-0 > .cmc-link > .sc-16r8icm-0 > .coin-logo");
-  await page.click("tr:nth-child(1) > td > .sc-16r8icm-0 > .cmc-link > .sc-16r8icm-0 > .coin-logo");
-
-  await navigationPromise;
-
-  await page.waitForSelector(".sc-16r8icm-0 > .sc-16r8icm-0 > .sc-10up5z1-0 > .sc-16r8icm-0:nth-child(1) > .content");
-  await page.click(".sc-16r8icm-0 > .sc-16r8icm-0 > .sc-10up5z1-0 > .sc-16r8icm-0:nth-child(1) > .content");
-
-  await page.waitForSelector(".sc-10up5z1-0 > .sc-16r8icm-0 > .content > li:nth-child(1) > .link-button");
-  await page.click(".sc-10up5z1-0 > .sc-16r8icm-0 > .content > li:nth-child(1) > .link-button");
-
-  const list = document.querySelector(`ul.content`);
+  const list = await page.$(`ul.content`);
   if (!list) throw new Error(`No list found`);
 
   const urls = getInterestingURLs(list);
   console.log(urls);
-
-  await browser.close();
+  await page.close();
 };
 
-function getInterestingURLs(element: Element) {
-  const urls = [] as string[];
-  const anchors = element.getElementsByTagName(`a`);
-  anchors?.length &&
-    [...anchors].forEach((anchor) => {
-      if (anchor.href.length) {
-        urls.push(anchor.href);
-      }
-    });
-  return urls;
+async function getInterestingURLs(element: ElementHandle<Element>) {
+  // const urls = [] as string[];
+
+  async function getProperty(element: ElementHandle, property: string) {
+    const p = await element.getProperty(property);
+    return await p?.jsonValue();
+  }
+
+  const href = await getProperty(element, "href");
+  return href;
+  // const anchors = element.getElementsByTagName(`a`);
+  // anchors?.length &&
+  //   [...anchors].forEach((anchor) => {
+  //     if (anchor.href.length) {
+  //       urls.push(anchor.href);
+  //     }
+  //   });
+  // return urls;
 }
