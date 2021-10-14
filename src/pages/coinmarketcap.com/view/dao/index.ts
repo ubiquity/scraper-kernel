48f37738d8a8453fa73c4dd8d2e1acaffa9ca104
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-13 23:51:46 -0500

    fix(typings): interface complaint fix

--- src/pages/coinmarketcap.com/view/dao/index.ts
+++ src/pages/coinmarketcap.com/view/dao/index.ts
@@ -1,5 +1,6 @@
 import puppeteer, { ElementHandle } from "puppeteer";
 import PageProps from "../../../../@types/page-props";
+import ScrapedProject from "../../../../@types/scraped-project";
 import { extractURLs, getMarketCap, getProperty, scrollToBottom } from "../../../../common";
 
 export default async (page: puppeteer.Page, pageLoad: Promise<puppeteer.HTTPResponse | null>) => {
@@ -14,13 +15,13 @@ export default async (page: puppeteer.Page, pageLoad: Promise<puppeteer.HTTPResp
   const { props } = JSON.parse(propsRawString) as PageProps;
   // console.log(props);
   // console.log(props.initialProps);
-  const data = props.initialProps.pageProps.info;
-  delete data.platforms;
-  delete data.relatedCoins;
-  delete data.relatedExchanges;
-  delete data.wallets;
-  delete data.holders;
-  console.log(JSON.stringify(data));
+  const token = props.initialProps.pageProps.info;
+  delete token.platforms;
+  delete token.relatedCoins;
+  delete token.relatedExchanges;
+  delete token.wallets;
+  delete token.holders;
+  console.log(JSON.stringify(token as ScrapedProject));
   // const marketCaps = await getMarketCaps(page);
   // console.log({ marketCaps });
 };
