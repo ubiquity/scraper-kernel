6efc0fdabd32dd7de39f9b5e26dd9c16d8733686
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-17 03:15:55 -0500

    fix(core): add missing files

---
+++ src/boot/new-tab-to-url.ts
@@ -0,0 +1,12 @@
+import puppeteer from "puppeteer";
+
+export default async function newTabToURL(browser: puppeteer.Browser, url: string) {
+  const destination = new URL(url);
+  if (!destination) {
+    throw new Error("No destination URL specified");
+  } else {
+    const page = await browser.newPage();
+    await page.goto(destination.href, { waitUntil: "networkidle2" });
+  }
+  return browser;
+}
