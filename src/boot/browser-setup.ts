fc962a95df6b1ac4196f756b51a2a5f7fff087e9
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-17 03:48:24 -0500

    fix(core): testing proxies doesnt look stable

--- src/boot/browser-setup.ts
+++ src/boot/browser-setup.ts
@@ -6,8 +6,8 @@ export const events = new EventEmitter();
 
 export default async function browserSetup(config: puppeteer.BrowserConnectOptions) {
   const browser = await puppeteer.launch(config);
+  browser.on("targetchanged", handlers.browserOnTargetChanged(browser));
   events.on("logicloaded", handlers.logicLoaded(browser));
   // events.on("logicfailed", handlers.logicFailed());
-  browser.on("targetchanged", handlers.browserOnTargetChanged(browser));
   return browser;
 }
--- src/common.ts
+++ src/common.ts
@@ -1,20 +1,10 @@
 import { ElementHandle, Page } from "puppeteer";
-
-export async function extractURLs(elements: ElementHandle<Element>[]) {
-  const urls = [] as string[];
-  for (const element of elements) {
-    const href = await getProperty(element, "href");
-    urls.push(href);
-  }
-  return urls;
-}
-
+export const getSourcedDate = () => new Date().toLocaleDateString();
+export const scrollToBottom = async (page: Page) => await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
 export async function getProperty(element: ElementHandle, query: string) {
   const property = await element.getProperty(query);
   return (await property?.jsonValue()) as string;
 }
-
-export const getSourcedDate = () => new Date().toLocaleDateString();
 export const getMarketCap = async (elements: ElementHandle<Element>[]) => {
   const mcaps = [] as string[];
   for (const element of elements) {
@@ -23,5 +13,11 @@ export const getMarketCap = async (elements: ElementHandle<Element>[]) => {
   }
   return mcaps;
 };
-
-export const scrollToBottom = async (page: Page) => await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+export async function extractURLs(elements: ElementHandle<Element>[]) {
+  const urls = [] as string[];
+  for (const element of elements) {
+    const href = await getProperty(element, "href");
+    urls.push(href);
+  }
+  return urls;
+}
