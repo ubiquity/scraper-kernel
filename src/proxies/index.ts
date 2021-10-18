5128c30109449091e7c4e7b30ac7fd3e5e5a781a
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-17 22:55:24 -0500

    enhancement(proxies): timeout works but waterfall fails

--- src/proxies/index.ts
+++ src/proxies/index.ts
@@ -1,5 +1,6 @@
 import ProxyHandler from "./proxy-handler";
 import { spysOneIps } from "./sources/spys-one";
+// TODO turn into a class
 export default async function proxies() {
   const ips = await spysOneIps();
   const handler = new ProxyHandler();
