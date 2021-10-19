a076447ceabae033897c9c053febcb6f19534ee2
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-18 21:54:51 -0500

    fix(jobs): dynamic function renamer now works

--- src/pages/coinmarketcap.com/view/dao/make-job-per-url.ts
+++ src/pages/coinmarketcap.com/view/dao/make-job-per-url.ts
@@ -1,17 +1,19 @@
 import puppeteer from "puppeteer";
-import { dynamicallyNameFunction } from "./dynamically-name-function";
-import { delegateRequestsToProxies } from "./delegate-requests-to-proxies";
 import { Job } from ".";
+import { delegateRequestsToProxies } from "./delegate-requests-to-proxies";
 
-export function makeJobPerURL(url: string, browser: puppeteer.Browser, proxyList: string[], jobs: Job[]) {
+export async function makeJobPerURL(url: string, browser: puppeteer.Browser, proxyList: string[], jobs: Job[]) {
   let name = url.split("/")[url.split("/").length - 2] as string | undefined;
   name = name?.match(/[a-z]+/gim)?.shift();
   if (!name) {
     name = "default";
   }
-  const unnamedJob = async () => await delegateRequestsToProxies(browser, url, proxyList);
 
-  const namedFunction = dynamicallyNameFunction(name, unnamedJob);
-  const job = namedFunction;
-  jobs.push(() => job);
+  jobs.push(
+    {
+      [name]: function () {
+        return delegateRequestsToProxies(browser, url, proxyList);
+      },
+    }[name]
+  );
 }
