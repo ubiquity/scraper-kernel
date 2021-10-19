eac192689dcbe4891ecea057189953493cd148ad
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-18 22:40:49 -0500

    fix(proxies): cycling seems to work

--- src/pages/coinmarketcap.com/view/dao/delegate-requests-to-proxies.ts
+++ src/pages/coinmarketcap.com/view/dao/delegate-requests-to-proxies.ts
@@ -1,9 +1,10 @@
 import puppeteer from "puppeteer";
 import useProxy from "puppeteer-page-proxy";
+import ScrapedProject from "../../../../@types/scraped-project";
 import { events } from "../../../../boot/browser-setup";
 import { projectScrape } from "./project-scrape";
 
-export async function delegateRequestsToProxies(browser: puppeteer.Browser, url: string, proxyList: string[], _page?: puppeteer.Page) {
+export async function delegateRequestsToProxies(browser: puppeteer.Browser, url: string, proxyList: string[], _page?: puppeteer.Page): Promise<ScrapedProject> {
   console.log(`setting up proxies for ${url}`);
   const proxy = proxyList.shift();
   if (!proxy) {
@@ -20,12 +21,26 @@ export async function delegateRequestsToProxies(browser: puppeteer.Browser, url:
   const page = _page || (await browser.newPage());
 
   const timer = setTimeout(() => events.emit("proxytimeout", () => delegateRequestsToProxies(browser, url, proxyList, page)), timeout);
-
+  console.log(`timer ${timer} started`);
   useProxy(page as object, `http://${proxy}`);
-  await page.goto(url);
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
-  clearTimeout(timer);
 
-  const token = await projectScrape(page);
-  return token;
+  try {
+    await page.goto(url);
+  } catch (e) {
+    _clearTimeout(timer);
+  }
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
+  try {
+    const token = await projectScrape(page);
+    _clearTimeout(timer);
+    return token;
+  } catch (e) {
+    _clearTimeout(timer);
+    return delegateRequestsToProxies(browser, url, proxyList, page);
+  }
+}
+
+function _clearTimeout(timer: NodeJS.Timeout) {
+  console.log(`timer ${timer} cleared`);
+  clearTimeout(timer);
 }
