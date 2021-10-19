0a3a175b82aa62a83a73aa9f236a07814ca9efce
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-18 23:11:44 -0500

    fix(proxies): still need to fix timeout

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { BrowserConnectOptions } from "puppeteer";
 export default {
   // slowMo: 10,
   devtools: false,
-  headless: true,
+  headless: false,
   defaultViewport: null,
   args: ["--lang=en-US,en;q=0.9"] as string[],
 } as BrowserConnectOptions;
--- src/pages/coinmarketcap.com/view/dao/delegate-requests-to-proxies.ts
+++ src/pages/coinmarketcap.com/view/dao/delegate-requests-to-proxies.ts
@@ -4,24 +4,27 @@ import ScrapedProject from "../../../../@types/scraped-project";
 import { events } from "../../../../boot/browser-setup";
 import { projectScrape } from "./project-scrape";
 
+const timeout = 5000;
+
 export async function delegateRequestsToProxies(browser: puppeteer.Browser, url: string, proxyList: string[], _page?: puppeteer.Page): Promise<ScrapedProject> {
-  console.log(`setting up proxies for ${url}`);
+  // console.log(`setting up proxies for ${url}`);
   const proxy = proxyList.shift();
   if (!proxy) {
     throw new Error("No proxy available");
   }
 
-  const timeout = 5000;
   console.log(`Connecting to "${url}" via "${proxy}"... timeout in ${timeout / 1000} seconds`);
 
   if (_page) {
     console.log(`\t...stopping page`);
-    _page.evaluate(() => stop()); // dont think that does anything, seems to still load
+    // _page.evaluate(() => stop()); // dont think that does anything, seems to still load
+    await _page.close();
   }
-  const page = _page || (await browser.newPage());
+  // const page = _page || (await browser.newPage());
+  const page = await browser.newPage();
 
   const timer = setTimeout(() => events.emit("proxytimeout", () => delegateRequestsToProxies(browser, url, proxyList, page)), timeout);
-  console.log(`timer ${timer} started`);
+  // console.log(`timer ${timer} started`);
   useProxy(page as object, `http://${proxy}`);
 
   try {
@@ -41,6 +44,6 @@ export async function delegateRequestsToProxies(browser: puppeteer.Browser, url:
 }
 
 function _clearTimeout(timer: NodeJS.Timeout) {
-  console.log(`timer ${timer} cleared`);
+  // console.log(`timer ${timer} cleared`);
   clearTimeout(timer);
 }
