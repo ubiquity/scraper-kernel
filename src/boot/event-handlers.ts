5b5d462cf0bc40da2ab79028abaccf9ca93058ac
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-20 05:05:15 -0500

    fix(proxies): cleaned up code still seems unstable

--- src/boot/event-handlers.ts
+++ src/boot/event-handlers.ts
@@ -1,13 +1,11 @@
-import fs from "fs";
-import path from "path";
-import { Browser, Handler, Target } from "puppeteer";
-import { events } from "./browser-setup";
+import { Browser } from "puppeteer";
+import { browserOnTargetChangedHandler } from "./events/browserOnTargetChanged";
 
-type PageLogic = (browser: Browser) => Promise<unknown[]>;
+export type PageLogic = (browser: Browser) => Promise<unknown[]>;
 
 export default eventHandlers;
 export const eventHandlers = {
-  proxyTimeout: function proxyTimeoutHandler(browser: Browser): (...args: any[]) => void {
+  proxyTimeout: function proxyTimeoutHandler(_browser: Browser): (...args: any[]) => void {
     return function setupProxies(callback) {
       callback();
     };
@@ -23,47 +21,5 @@ export const eventHandlers = {
    * It will load the logic for the page and then call the logic.
    * @param browser the browser instance
    */
-  browserOnTargetChanged: function browserOnTargetChangedHandler(browser: Browser): Handler<any> {
-    return async (target: Target) => {
-      const url = target.url();
-
-      const logic = importLogic(url);
-      const defaultExport = (await logic)?.default as PageLogic | undefined;
-
-      if (!defaultExport) {
-        console.log(`\t...No logic found`);
-        // target.page().then((page) => page?.close());
-        // events.emit("logicfailed", url);
-        return;
-      }
-
-      const page = await target.page();
-      if (page) {
-        page.waitForNavigation({ waitUntil: "networkidle2" }).then(() => events.emit("logicloaded", defaultExport));
-      } else {
-        console.error(`No page found for ${url}`);
-        return;
-      }
-    };
-  },
+  browserOnTargetChanged: browserOnTargetChangedHandler,
 };
-
-export function importLogic(url: string) {
-  const selection = url.split("://").pop();
-  if (!selection) {
-    throw new Error("Page URL parse error");
-  }
-  const pathTo = path.join(process.cwd(), "dist", "pages", selection);
-  const exists = fs.existsSync(pathTo);
-  if (exists) {
-    const logic = import(pathTo).catch((error) => {
-      console.error(error);
-      console.error(`Import page logic error for ${selection}`);
-    });
-    return logic as Promise<Module>;
-  }
-}
-
-interface Module {
-  default?: Function;
-}
