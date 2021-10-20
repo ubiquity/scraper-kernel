e429b563411feaf2bcc469548e095a6a720e53c0
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-20 06:29:39 -0500

    fix(core): default headless browsing

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { BrowserConnectOptions } from "puppeteer";
 export default {
   // slowMo: 10,
   devtools: false,
-  headless: process.env.DEBUG_HEADLESS || false,
+  headless: process.env.DEBUG_HEADFUL || true,
   defaultViewport: null,
   args: ["--lang=en-US,en;q=0.9"] as string[],
 } as BrowserConnectOptions;
