2b0ff80a9a35062525c2da6c7147971f8fc4fc78
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-10-28 07:27:13 -0500

    test(jimp): modularized

--- src/pages/web.telegram.org/z/jimp.ts
+++ src/pages/web.telegram.org/z/jimp.ts
@@ -3,25 +3,25 @@ import fs from "fs";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const qrCode = require("qrcode-reader");
 
-const buffer = fs.readFileSync(__dirname + "/index.png");
-if (!buffer) {
-  throw new Error("Could not read image");
-}
-// console.log(buffer.byteLength, "bytes read");
-export default new Promise((resolve, reject) => {
-  Jimp.read(buffer, callback);
+export default (buffer: Buffer) => {
+  if (!buffer) {
+    buffer = fs.readFileSync(__dirname + "/index.png");
+  }
+  return new Promise((resolve, reject) => {
+    Jimp.read(buffer, callback);
 
-  function callback(err: any, image: { bitmap: any }) {
-    if (err) {
-      reject(err);
-    }
-    const qrcode = new qrCode();
-    qrcode.callback = function (err: any, value: { result: any }) {
+    function callback(err: any, image: { bitmap: any }) {
       if (err) {
         reject(err);
       }
-      resolve(value.result);
-    };
-    qrcode.decode(image.bitmap);
-  }
-});
+      const qrcode = new qrCode();
+      qrcode.callback = function (err: any, value: { result: any }) {
+        if (err) {
+          reject(err);
+        }
+        resolve(value.result);
+      };
+      qrcode.decode(image.bitmap);
+    }
+  });
+};
