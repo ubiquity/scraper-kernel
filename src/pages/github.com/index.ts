075fa66b12882724ff7c308e4c06a88e7a1d030d
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-14 19:07:55 +0800

    feature(github): scrapes contribs in the last year, and returns only the integer

--- src/pages/github.com/index.ts
+++ src/pages/github.com/index.ts
@@ -14,7 +14,14 @@ export default async (browser: puppeteer.Browser) => {
   });
 
   page.on("response", async (response) => console.log("<<", response.status(), response.url()));
-  const contribs = await page.waitForSelector(`#js-contribution-activity`);
-  console.log(contribs);
-  return contribs;
+
+  const one = await page.$(
+    `#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-main > div:nth-child(2) > div > div.mt-4.position-relative > div > div.col-12.col-lg-10 > div.js-yearly-contributions > div:nth-child(1) > h2`
+  );
+  //    eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //    @ts-ignore-error
+  const two = await (await one.getProperty("textContent")).jsonValue();
+  //    eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //    @ts-ignore-error
+  return Number(two.match(/[0-9]*/gim).join(``));
 };
