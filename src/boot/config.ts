f2f2b869dfe15f925f84dcaa28a96f8f63c1de77
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-14 19:48:16 +0800

    fix(config): headless and headful

--- src/boot/config.ts
+++ src/boot/config.ts
@@ -1,9 +1,10 @@
 import { BrowserConnectOptions } from "puppeteer";
-
+const headful = Boolean(process.env.DEBUG_HEADFUL);
+console.log(headful);
 export default {
   // slowMo: 10,
   devtools: false,
-  headless: !process.env.DEBUG_HEADFUL,
+  headless: !headful,
   defaultViewport: null,
   args: ["--lang=en-US,en;q=0.9"] as string[],
   cache: "../cache",
