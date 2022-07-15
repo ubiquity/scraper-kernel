d92f9f349f920a6094802f5ef73637aa1c5e6f63
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-15 13:19:19 +0800

    enhancement(all): simplify codebase - about to attempt parallelization again

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,24 +1,29 @@
-import dotenv from "dotenv";
 import { EventEmitter } from "events";
 import "source-map-support/register";
-import { attachEvents } from "./boot/events/attachEvents";
 import browserSetup from "./boot/browser-setup";
 import config from "./boot/config";
 import { eventHandlers } from "./boot/event-handlers";
+import { attachEvents } from "./boot/events/attachEvents";
 import newTabToURL from "./boot/new-tab-to-url";
-dotenv.config();
 
-export const events = new EventEmitter();
+let _userInput: string;
 
-export default async function scrape(userInput: string) {
-  const browser = await browserSetup(config); // Setup browser and listen for events
-  attachEvents(browser);
-  newTabToURL(browser, userInput);
+browserSetup(config)
+  .then((browser) => attachEvents(browser))
+  .then((browser) => {
+    console.log({ _userInput });
+    newTabToURL(browser, _userInput);
+  });
+
+export default function scrape(userInput: string) {
+  _userInput = userInput;
   const promise = new Promise(magicMysteryPromise);
   return promise;
 }
 
 type ResolveFunction = (results: string) => void;
+
+export const events = new EventEmitter();
 
 function magicMysteryPromise(resolve: ResolveFunction): void {
   events.on("scrapecomplete", eventHandlers.scrapeComplete(resolve));
