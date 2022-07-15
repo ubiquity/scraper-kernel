2b0f591de342d8ec47620035b5efed6318affbc2
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-15 12:48:17 +0800

    fix(exit): clean exits useful for ci

--- src/cli.ts
+++ src/cli.ts
@@ -2,5 +2,11 @@ const homePage = process.argv[2];
 import scrape from "./scrape";
 // CLI ADAPTER
 scrape(homePage)
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+  .then((data) => {
+    console.log(data);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
