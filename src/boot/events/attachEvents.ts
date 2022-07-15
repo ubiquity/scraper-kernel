dab4d4f30541675b04d8edc30a6d1c025141ed27
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-15 13:19:19 +0800

    enhancement(all): simplify codebase - about to attempt parallelization again

--- src/boot/events/attachEvents.ts
+++ src/boot/events/attachEvents.ts
@@ -1,10 +1,11 @@
 import { Browser } from "puppeteer";
-import { eventHandlers } from "../event-handlers";
 import { events } from "../../scrape";
+import { eventHandlers } from "../event-handlers";
 
 export function attachEvents(browser: Browser) {
   browser.on("targetchanged", eventHandlers.browserOnTargetChanged(browser));
   events.on("proxytimeout", eventHandlers.proxyTimeout(browser));
   events.on("logicloaded", eventHandlers.logicLoaded(browser));
   // events.on("logicfailed", eventHandlers.logicFailed(browser));
+  return browser;
 }
