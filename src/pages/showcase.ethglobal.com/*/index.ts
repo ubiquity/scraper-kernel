aa3b22c0e9de86bcf64a5e869d772eb4a4e4b120
アレクサンダー.eth <gpg@pavlovcik.com>
2022-09-22 20:24:29 +0800

    feature: gets githubs

--- src/pages/showcase.ethglobal.com/*/index.ts
+++ src/pages/showcase.ethglobal.com/*/index.ts
@@ -1,28 +1,23 @@
 import puppeteer from "puppeteer";
+import { getProperty } from "../../../common";
 export default async (browser: puppeteer.Browser) => {
-  const page = await setupHackathonScraper(browser);
-  const githubRepo = (await page.$(`a[href*=github]`)) as HTMLAnchorElement | null;
-
-  if (githubRepo?.href) {
-    console.log(githubRepo.href);
-    return githubRepo.href;
+  const page = await getPage(browser);
+  const githubSelector = `a[href*=github]`;
+  const anchor = await page.waitForSelector(githubSelector);
+  if (anchor) {
+    const href = await getProperty(anchor, "href");
+    if (href) {
+      return href;
+    }
   }
 };
 
-async function setupHackathonScraper(browser: puppeteer.Browser) {
+async function getPage(browser: puppeteer.Browser) {
   const pages = await browser.pages();
   const page = pages[pages.length - 1];
   if (!page) {
     throw new Error("No page found");
   }
 
-  await page.setRequestInterception(true);
-
-  page.on("request", (request) => {
-    console.log(">>", request.method(), request.url());
-    request.continue();
-  });
-
-  page.on("response", (response) => console.log("<<", response.status(), response.url()));
   return page;
 }
