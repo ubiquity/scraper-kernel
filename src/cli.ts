df561f1000ac6d8b483a772c515c794fca5ab47e
アレクサンダー.eth <gpg@pavlovcik.com>
2022-09-23 18:41:45 +0800

    feature: stream csv output

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,11 @@
 const userInput = process.argv.slice(2);
 import entryPoint from "./scrape";
+import fs from "fs";
 // CLI ADAPTER
 entryPoint(userInput)
   .then((data) => {
     console.log(`<<`, data);
+    fs.writeFileSync("results.json", JSON.stringify(data));
     process.exit(0);
   })
   .catch((err) => {
--- src/pages/showcase.ethglobal.com/*/index.ts
+++ src/pages/showcase.ethglobal.com/*/index.ts
@@ -6,13 +6,18 @@ export default async (browser: puppeteer.Browser) => {
   const page = await getPage(browser);
   const githubSelector = `a[href*=git]`; // I noticed gitlab and etherscan links for source code
   // const anchor = await page.waitForSelector(githubSelector, { timeout: 5000 }).catch((error) => console.error(`Couldn't find Git link`, error));
-  const anchor = await page.$(githubSelector).catch((error) => console.error(`Couldn't find Git link`, error));
-  if (anchor) {
-    const href = await getProperty(anchor, "href");
-    if (href) {
-      // console.log({ href });
-      fs.appendFile("buffer.log", href.concat("\n"), (error) => console.error(error));
-      return href;
+  const button = await page.$(githubSelector).catch((error) => error && console.error(`Couldn't find Git link`, error));
+  if (button) {
+    const githubUrl = await getProperty(button, "href");
+    if (githubUrl) {
+      const row = [
+        Date.now(), // timestamp
+        page.url().replace("https://showcase.ethglobal.com/", "").split("/"), // page url
+        githubUrl, // github url
+      ].join(",");
+
+      fs.appendFile("buffer.csv", row.concat("\n"), (error) => console.error(error));
+      return githubUrl;
     }
   }
 };
