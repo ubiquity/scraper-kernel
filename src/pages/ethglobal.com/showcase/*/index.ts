1aa8c1a1fd11169c8fc4f8709689c3f270b61ada
アレクサンダー.eth <gpg@pavlovcik.com>
2022-09-27 20:55:40 +0800

    unstable

--- src/pages/ethglobal.com/showcase/*/index.ts
+++ src/pages/ethglobal.com/showcase/*/index.ts
@@ -3,24 +3,25 @@ import fs from "fs";
 import { getActiveTab, getProperty, colorizeText } from "../../../../utils";
 export default async (browser: puppeteer.Browser) => {
   console.log(colorizeText(`logic executing for ${__filename}`, "fgMagenta"));
-  const page = await getActiveTab(browser);
-  const githubSelector = `a[href*=git]`; // I noticed gitlab and etherscan links for source code
-  // const anchor = await page.waitForSelector(githubSelector, { timeout: 5000 }).catch((error) => console.error(`Couldn't find Git link`, error));
-  const button = await page.$(githubSelector).catch((error) => error && console.error(`Couldn't find Git link`, error));
+  return "test";
+  // const page = await getActiveTab(browser);
+  // const githubSelector = `a[href*=git]`; // I noticed gitlab and etherscan links for source code
+  // // const anchor = await page.waitForSelector(githubSelector, { timeout: 5000 }).catch((error) => console.error(`Couldn't find Git link`, error));
+  // const button = await page.$(githubSelector).catch((error) => error && console.error(`Couldn't find Git link`, error));
 
-  if (button) {
-    const githubUrl = await getProperty(button, "href");
-    if (githubUrl && !githubUrl.includes("ethglobal")) {
-      const row = [
-        Date.now(), // timestamp
-        page.url().replace("https://ethglobal.com/showcase/", "").split("/"), // page url
-        githubUrl, // github url
-      ].join(",");
+  // if (button) {
+  //   const githubUrl = await getProperty(button, "href");
+  //   if (githubUrl && !githubUrl.includes("ethglobal")) {
+  //     const row = [
+  //       Date.now(), // timestamp
+  //       page.url().replace("https://ethglobal.com/showcase/", "").split("/"), // page url
+  //       githubUrl, // github url
+  //     ].join(",");
 
-      fs.appendFile("buffer.csv", row.concat("\n"), (error) => error && console.error(error));
-      return githubUrl;
-    }
-  } else {
-    return null;
-  }
+  //     fs.appendFile("buffer.csv", row.concat("\n"), (error) => error && console.error(error));
+  //     return githubUrl;
+  //   }
+  // } else {
+  //   return null;
+  // }
 };
