19d0552f8979662968b01b69e38b3e28f5b046f3
アレクサンダー.eth <gpg@pavlovcik.com>
2022-10-02 16:07:08 +0800

    fix(logic loader): loads according to designed strategy but needs refinement

--- src/boot/events/search-for-import.ts
+++ src/boot/events/search-for-import.ts
@@ -6,36 +6,44 @@ import { PageLogic } from "../event-handlers";
 export type DestinationStrategy = (destination: string) => string;
 
 const cwd = process.cwd();
+const safetyBuffer = path.basename(path.join(cwd, ".."));
 
 // @TODO: loading logic should recurse up directory parents and replace them with asterisks
 // e.g. github.com/*/* should be considered for repos
 
-let failsafe = 10;
+let failsafe = 30;
 
-export async function searchForImport(importing: string, savedPosition?: string): Promise<PageLogic> {
+export async function searchForImport(importing: string, startPosition?: string): Promise<PageLogic> {
+  return await _searchForImport(importing, startPosition ? startPosition : importing);
+}
+
+async function _searchForImport(importing: string, startPosition: string) {
+  // if (!savedPosition) {
+  //   // THE REQUESTED IMPORT PATH IS OUTSIDE OF THE PROJECT DIRECTORY, WHICH IS INVALID
+  //   throw new Error("the requested page logic import path is outside of the project directory, which is invalid");
+  // } else {
   if (!--failsafe) {
     throw new Error("infinite loop?");
   }
   // console.log(colorizeText(`recursion!`, "fgWhite"));
   if (!importing.includes(cwd)) {
     log.error(`out of bounds`);
-    if (!savedPosition) {
-      // THE REQUESTED IMPORT PATH IS OUTSIDE OF THE PROJECT DIRECTORY, WHICH IS INVALID
-      throw new Error("the requested page logic import path is outside of the project directory, which is invalid");
-    } else {
-      importing = path.resolve(savedPosition, ".."); // go up one
-      log.warn(importing);
-    }
+    importing = startPosition = path.resolve(startPosition, ".."); // go up one
+    // importing = path.resolve(startPosition);
   }
 
+  // log.warn(`[${startPosition}] startPosition`);
+  // log.warn(`[${importing}] up to parent`);
+  // }
   const logic = (await checkModifier(importing, "index.js")) || (await checkModifier(importing, "*"));
 
   if (logic) {
     return logic;
   } else {
-    const startPosition = importing;
+    // const startPosition = importing;
     const wildCardPath = renameLastPartOfPathToWildCard(importing); // pathname ends with */*
-    return await searchForImport(wildCardPath, startPosition);
+    // console.log(wildCardPath);
+    return await _searchForImport(wildCardPath, startPosition);
   }
 }
 
@@ -51,8 +59,18 @@ function renameLastPartOfPathToWildCard(query: string) {
       break;
     }
   }
-  let resolved = path.resolve(...pathParts);
-  resolved = resolved.replace(process.cwd(), "");
+  // let resolved = path.resolve(...pathParts);
+  const resolved = pathParts.join(path.sep);
+
+  // console.log(resolved, safetyBuffer);
+
+  if (!resolved.includes(safetyBuffer)) {
+    // console.log(pathParts); // check if only cwd remain. if so then we ran out of options and throw error
+    //   // THE REQUESTED IMPORT PATH IS OUTSIDE OF THE PROJECT DIRECTORY, WHICH IS INVALID
+    throw new Error("the requested page logic import path is outside of the project directory, which is invalid");
+  }
+
+  // resolved = resolved.replace(cwd, "");
 
   // console.warn(pathParts);
   // log.warn(pathParts.join(path.sep));
