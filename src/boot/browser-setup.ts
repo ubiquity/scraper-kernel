53a1d6f6bbb84ec81cfbe14a36ea761aacdec534
アレクサンダー.eth <gpg@pavlovcik.com>
2023-01-05 15:55:24 +0800

    remove: kernel

--- src/boot/browser-setup.ts
+++
@@ -1,5 +0,0 @@
-import puppeteer from "puppeteer";
-
-export default async function browserSetup(config: puppeteer.BrowserConnectOptions): Promise<puppeteer.Browser> {
-  return await puppeteer.launch(config);
-}
