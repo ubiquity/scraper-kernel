98bc0bfac5e82fd27e47e650d14fec18370de9de
アレクサンダー.eth <gpg@pavlovcik.com>
2023-01-28 15:09:59 +0800

    fix(headful): made a more robust function to set headful mode on

--- src/boot/config.ts
+++ src/boot/config.ts
@@ -6,23 +6,23 @@ dotenv.config();
 
 export default {
   devtools: false,
-  headless: isHeadful(),
+  headless: isHeadless(),
   defaultViewport: null,
   args: ["--lang=en-US,en;q=0.9"] as string[],
   cache: "../cache",
 } as BrowserConnectOptions;
 
-function isHeadful() {
+function isHeadless() {
   // the default settings are that the browser is headless (aka headful is false)
   // first we should check the command line arguments. if the user has passed in the --headful flag, then we should set headless to false (aka headful is true)
   const headful = readCommandLineArgs?.headful;
   if (headful) {
-    return true;
+    return false;
   }
   // if the user has not passed in the --headful flag, then we should check the DEBUG_HEADFUL environment variable. if the user has set DEBUG_HEADFUL to true, then we should set headless to false (aka headful is true)
   const env = process.env.DEBUG_HEADFUL;
   if (env === "true") {
-    return true;
+    return false;
   }
-  return false;
+  return true;
 }
