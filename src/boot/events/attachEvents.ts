c8f756c61667cc7e0ad4b711535a7a6999ca1982
アレクサンダー.eth <gpg@pavlovcik.com>
2023-01-31 17:59:48 +0800

    fix: syntax errors related to passing in pagesDirectory

--- src/boot/events/attachEvents.ts
+++ src/boot/events/attachEvents.ts
@@ -2,10 +2,12 @@ import { Browser } from "puppeteer";
 import { eventEmitter } from "../../scrape";
 import { eventHandlers } from "../event-handlers";
 
-export function attachEvents(browser: Browser) {
-  browser.on("targetchanged", eventHandlers.browserOnTargetChanged(browser));
-  eventEmitter.on("proxytimeout", eventHandlers.proxyTimeout(browser));
+export function attachEvents(browser: Browser, pagesDirectory: string) {
+  const browserOnTargetChanged = eventHandlers.setupBrowserOnTargetChanged(browser, pagesDirectory);
+
+  browser.on("targetchanged", browserOnTargetChanged);
+  eventEmitter.on("proxytimeout", eventHandlers.proxyTimeout());
   eventEmitter.on("logicloaded", eventHandlers.logicLoaded(browser));
-  eventEmitter.on("logicfailed", eventHandlers.logicFailed(browser));
+  eventEmitter.on("logicfailed", eventHandlers.logicFailed());
   return browser;
 }
--- src/scrape.ts
+++ src/scrape.ts
@@ -3,48 +3,47 @@ import { Browser } from "puppeteer";
 import "source-map-support/register";
 import browserSetup from "./boot/browser-setup";
 import config from "./boot/config";
-import renderEventHandlers from "./boot/event-handlers";
+import { eventHandlers } from "./boot/event-handlers";
 import { attachEvents } from "./boot/events/attachEvents";
 import newTabToURL from "./boot/new-tab-to-url";
 
 export const eventEmitter = new EventEmitter();
-export type JobResult = Error | string | null; // definitely string, but not sure if `void` or `null` is correct to signal no results found
+export type JobResult = Error | string | null;
 
 export default async function scrape(
   urls: string[] | string,
   pagesDirectory: string, // page logic directory path
   browser?: Browser
-  // , concurrency?: number
 ): Promise<JobResult | JobResult[]> {
   if (!pagesDirectory) {
     throw new Error("Need page logic path");
   }
 
-  browser = await attachEventsOnFirstRun(browser);
+  browser = await attachEventsOnFirstRun(pagesDirectory, browser);
 
   if (typeof urls === "string") {
-    const singleResult = await _scrapeSingle(urls, browser, pagesDirectory);
+    const singleResult = await _scrapeSingle(urls, browser);
     return singleResult;
   } else if (Array.isArray(urls)) {
-    const seriesResults = await _scrapeSeries(urls, browser, pagesDirectory);
+    const seriesResults = await _scrapeSeries(urls, browser);
     return seriesResults;
   } else {
     throw new Error("`urls` must be of types `string[] | string` ");
   }
 }
 
-export async function _scrapeSeries(urls: string[], browser: Browser, pagesDirectory: string): Promise<JobResult[]> {
+export async function _scrapeSeries(urls: string[], browser: Browser): Promise<JobResult[]> {
   const completedScrapes = [] as JobResult[];
   for (const url of urls) {
-    completedScrapes.push(await _scrapeSingle(url, browser, pagesDirectory));
+    completedScrapes.push(await _scrapeSingle(url, browser));
   }
   return completedScrapes;
 }
 
 type ResolveFunction = (results: string) => void;
-export async function _scrapeSingle(url: string, browser: Browser, pagesDirectory: string): Promise<JobResult | Error> {
+export async function _scrapeSingle(url: string, browser: Browser): Promise<JobResult | Error> {
   const scrapeJob = new Promise(function addCallbackEvent(resolve: ResolveFunction, reject): void {
-    eventEmitter.once("scrapecomplete", renderEventHandlers(pagesDirectory).scrapeComplete(resolve, reject));
+    eventEmitter.once("scrapecomplete", eventHandlers.scrapeComplete(resolve, reject));
   });
   console.log(`>>`, url); // useful to follow headless page navigation
   const { page, response } = await newTabToURL(browser, url);
@@ -59,10 +58,10 @@ export async function _scrapeSingle(url: string, browser: Browser, pagesDirector
   return results;
 }
 
-async function attachEventsOnFirstRun(browser?: Browser) {
+async function attachEventsOnFirstRun(pagesDirectory: string, browser?: Browser) {
   if (!browser) {
     browser = await browserSetup(config);
-    attachEvents(browser);
+    attachEvents(browser, pagesDirectory);
   }
   return browser;
 }
