db3a78c7bdebe96dc2da7422e7d7807699474945
アレクサンダー.eth <gpg@pavlovcik.com>
2023-01-31 17:45:38 +0800

    fix: event handlers export

--- src/boot/event-handlers.ts
+++ src/boot/event-handlers.ts
@@ -5,42 +5,39 @@ import { browserOnTargetChangedHandler } from "./events/browserOnTargetChanged";
 
 export type PageLogic = (browser: Browser, page: Page) => Promise<string[]>;
 
-export default function renderEventHandlers(pagesDirectory: string) {
-  return {
-    proxyTimeout: function proxyTimeoutHandler(_browser: Browser): (...args: any[]) => void {
-      return function setupProxies(callback) {
-        callback();
-      };
-    },
-    logicLoaded: function logicLoadedHandler(browser: Browser): (...args: any[]) => void {
-      return async function _logicLoadedHandler(logic: PageLogic, page: Page) {
-        return await logic(browser, page);
-      };
-    },
-    logicFailed: function logicFailedHandler(browser: Browser): (...args: any[]) => void {
-      return function _logicFailedHandler(error: Error) {
-        log.error(error.message);
-      };
-    },
+export const eventHandlers = {
+  proxyTimeout: function proxyTimeoutHandler(_browser: Browser): (...args: any[]) => void {
+    return function setupProxies(callback) {
+      callback();
+    };
+  },
+  logicLoaded: function logicLoadedHandler(browser: Browser): (...args: any[]) => void {
+    return async function _logicLoadedHandler(logic: PageLogic, page: Page) {
+      return await logic(browser, page);
+    };
+  },
+  logicFailed: function logicFailedHandler(browser: Browser): (...args: any[]) => void {
+    return function _logicFailedHandler(error: Error) {
+      log.error(error.message);
+    };
+  },
 
-    scrapeComplete: function scrapeCompleteHandler(resolve, reject) {
-      return async function _scrapeCompleteHandler(results: JobResult) {
-        if (!results) {
-          reject("no results");
-        }
-        // console.trace({ results: await results });
-        // debugger;
-        return resolve(results);
-      };
-    },
+  scrapeComplete: function scrapeCompleteHandler(resolve, reject) {
+    return async function _scrapeCompleteHandler(results: JobResult) {
+      if (!results) {
+        reject("no results");
+      }
+      // console.trace({ results: await results });
+      // debugger;
+      return resolve(results);
+    };
+  },
 
-    /**
-     * This is the main handler that will be called when the browser navigates to any new page.
-     * It will load the logic for the page and then call the logic.
-     * @param browser the browser instance
-     */
-    browserOnTargetChanged: (browser: Browser) => browserOnTargetChangedHandler(browser, pagesDirectory),
-  };
-}
-
-// export default renderEventHandlers;
+  /**
+   * This is the main handler that will be called when the browser navigates to any new page.
+   * It will load the logic for the page and then call the logic.
+   * @param browser the browser instance
+   */
+  browserOnTargetChanged: browserOnTargetChangedHandler,
+};
+export default eventHandlers;
