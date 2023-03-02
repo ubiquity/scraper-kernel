30817e0f0b5b2ffb4f693855c7293bfdfd88d649
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-02 18:23:02 +0800

    test(ci): pass in path to chrome

--- src/boot/browser-setup.ts
+++ src/boot/browser-setup.ts
@@ -2,10 +2,5 @@ import puppeteer from "puppeteer";
 import { Browser, BrowserLaunchArgumentOptions } from "puppeteer";
 
 export default async function browserSetup(config: BrowserLaunchArgumentOptions): Promise<Browser> {
-  // config.browserURL = "http://127.0.0.1:9222";
-
-  // const browser = await puppeteer.connect(config);
-  // return browser;
-
   return await puppeteer.launch(config);
 }
