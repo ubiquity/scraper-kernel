82eeeec5151cc0c339de9b377ee72033d3ec68f7
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-02 18:06:31 +0800

    feat: connect to running instance

--- src/boot/browser-setup.ts
+++ src/boot/browser-setup.ts
@@ -2,5 +2,10 @@ import puppeteer from "puppeteer";
 import { Browser, BrowserLaunchArgumentOptions } from "puppeteer";
 
 export default async function browserSetup(config: BrowserLaunchArgumentOptions): Promise<Browser> {
-  return await puppeteer.launch(config);
+  config.browserURL = "http://localhost:9222";
+
+  const browser = await puppeteer.connect(config);
+  return browser;
+
+  // return await puppeteer.launch(config);
 }
