76a73ce84913bbad9e223cc0ec3287ad95df350d
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-02 18:31:35 +0800

    test(ci): pass in path to chrome

--- src/boot/config.ts
+++ src/boot/config.ts
@@ -3,13 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function setupConfig(args) {
+  const selectedExecutablePath = args.executablePath || process.env.PUPPETEER_EXECUTABLE_PATH;
+  console.trace({ selectedExecutablePath });
   const config = {
     devtools: false,
     headless: isHeadless(args),
     defaultViewport: null,
     args: ["--lang=en-US,en;q=0.9"] as string[],
     cache: "../cache",
-    executablePath: args.executablePath || process.env.PUPPETEER_EXECUTABLE_PATH,
+    executablePath: selectedExecutablePath,
   } as BrowserLaunchArgumentOptions;
 
   if (args.chromium) {
