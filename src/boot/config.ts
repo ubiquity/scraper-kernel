3d59d2cc82b8a7b3e6183eb0612df42460546d83
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-02 11:34:53 +0800

    feat: dotenv

--- src/boot/config.ts
+++ src/boot/config.ts
@@ -1,5 +1,6 @@
 import { BrowserLaunchArgumentOptions } from "puppeteer";
-
+import dotenv from "dotenv";
+dotenv.config();
 export function setupConfig(args) {
   const config = {
     devtools: false,
@@ -7,7 +8,7 @@ export function setupConfig(args) {
     defaultViewport: null,
     args: ["--lang=en-US,en;q=0.9"] as string[],
     cache: "../cache",
-    executablePath: process.env.PUPPETEER_EXEC_PATH
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
   } as BrowserLaunchArgumentOptions;
 
   if (args.chromium) {
