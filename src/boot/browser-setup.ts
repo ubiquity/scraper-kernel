f36dd46968b1fb7b5b26f63e184d48514dfa0348
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-02 18:22:26 +0800

    test(ci): pass in path to chrome

--- src/boot/browser-setup.ts
+++ src/boot/browser-setup.ts
@@ -2,10 +2,10 @@ import puppeteer from "puppeteer";
 import { Browser, BrowserLaunchArgumentOptions } from "puppeteer";
 
 export default async function browserSetup(config: BrowserLaunchArgumentOptions): Promise<Browser> {
-  config.browserURL = "http://localhost:9222";
+  // config.browserURL = "http://127.0.0.1:9222";
 
-  const browser = await puppeteer.connect(config);
-  return browser;
+  // const browser = await puppeteer.connect(config);
+  // return browser;
 
-  // return await puppeteer.launch(config);
+  return await puppeteer.launch(config);
 }
--- src/boot/config.ts
+++ src/boot/config.ts
@@ -1,9 +1,6 @@
 import { BrowserLaunchArgumentOptions } from "puppeteer";
 import dotenv from "dotenv";
 dotenv.config();
-const executablePath = "/home/runner/work/scraper-parent-test/scraper-parent-test/chrome-linux/chrome";
-
-console.log({ executablePath });
 
 export function setupConfig(args) {
   const config = {
@@ -12,7 +9,7 @@ export function setupConfig(args) {
     defaultViewport: null,
     args: ["--lang=en-US,en;q=0.9"] as string[],
     cache: "../cache",
-    executablePath,
+    executablePath: args.executablePath || process.env.PUPPETEER_EXECUTABLE_PATH,
   } as BrowserLaunchArgumentOptions;
 
   if (args.chromium) {
